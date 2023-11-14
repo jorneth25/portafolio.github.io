700b44e3ff545089e51c654567872a6030b4d537
Jorno25 <kyoshosushidelivery@gmail.com>
2023-11-13 22:38:46 -0400

    Solving issues and links

--- main.js
+++ main.js
@@ -12,3 +12,40 @@ window.addEventListener("scroll", function () {
 
   prevScrollPos = currScrollPos;
 });
+
+
+const bloque = document.querySelector("footer");
+
+bloque.addEventListener("click", () =>{
+  bloque.style.backgroundColor = 'blue'
+});
+
+
+const bloqueWhoIAm= document.querySelector(".mydescription-container")
+const bloqueWhatIDo = document.querySelector(".moredescription")
+const descripcionButton = document.querySelector(".readMoreBtn");
+const backDescripcionButton = document.querySelector(".backBtn");
+
+bloqueWhatIDo.hidden = true
+
+function showMoreAboutMe (){
+  bloqueWhoIAm.hidden=true;
+  bloqueWhatIDo.hidden=false;
+}
+
+function backToWhoIAm (){
+  bloqueWhatIDo.hidden=true;
+  bloqueWhoIAm.hidden=false;
+}
+
+/*Tester 
+
+function changeBackground (){
+  backDescripcionButton.style.backgroundColor = 'green';
+
+}
+
+
+descripcionButton.addEventListener("mouseenter", (changeBackground));*/
+descripcionButton.addEventListener('click',(showMoreAboutMe));
+backDescripcionButton.addEventListener('click',(backToWhoIAm));
